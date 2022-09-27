972784be5919b75f7d007ced881d05e576dd564e
Jebeniani <aymenjebeniani05@gmail.com>
2022-09-27 23:30:35 +0100

    subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Number of subs"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Number of subscribers
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    r = requests.get(url.format(subreddit), headers=headers)
+    try:
+        subscribers = r.json().get("data").get("subscribers")
+        return subscribers
+    except:
+        return 0
